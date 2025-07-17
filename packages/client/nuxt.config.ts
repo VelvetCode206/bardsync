f199a5085dfdb5b64d6b480962240f3ea998e920
jiaf-gh <77030734+jiaf-gh@users.noreply.github.com>
2025-07-16 22:58:25 -0300

    feat(client): added basic metadata properties

--- packages/client/nuxt.config.ts
+++ packages/client/nuxt.config.ts
@@ -6,6 +6,25 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: { tsConfig: { extends: resolve(dirname(fileURLToPath(import.meta.url)), '../../tsconfig.base.json') } },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en-US' },
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        { name: 'description', content: 'This is a template Nuxt application' },
+      ],
+      title: 'Nuxt App',
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      }],
+    },
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
